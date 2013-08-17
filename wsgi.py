4d4e2d5deb9fb4b1ed1dff7c5d7469ebbf326b06
Mingcai SHEN <archsh@gmail.com>
2013-08-18 01:37:56 +0800

    Updated.

--- wsgi.py
+++ wsgi.py
@@ -5,29 +5,23 @@ from restlet.application import RestletApplication
 
 DEFAULT_VCAP_SERVICES = """\
 {
-    postgresql-9.1: [
-        {
-            name: "pqdb",
-            label: "postgresql-9.1",
-            plan: "free",
-            tags: [
-                "postgresql",
-                "postgresql-9.1",
-                "relational",
-                "postgresql-9.1",
-                "postgresql"
-            ],
-            credentials: {
-                name: "d01d2c6f92ad74c3e929653933714ab4a",
-                host: "10.0.35.83",
-                hostname: "10.0.35.83",
-                port: 5432,
-                user: "u10f197a24e4e485b8b96cbe6dbac8c8f",
-                username: "u10f197a24e4e485b8b96cbe6dbac8c8f",
-                password: "pa17ae6a8ed5140948df45e533af83b69"
-            }
-        }
-    ]
+ "postgresql-9.1":[
+    {
+      "name":"pqdb",
+      "label":"postgresql-9.1",
+      "plan":"free",
+      "tags":["postgresql","postgresql-9.1","relational","postgresql-9.1","postgresql"],
+      "credentials":{
+        "name":"d01d2c6f92ad74c3e929653933714ab4a",
+        "host":"10.0.35.83",
+        "hostname":"10.0.35.83",
+        "port":5432,
+        "user":"ua4539079654d4a8c838b8bfd105a9db0",
+        "username":"ua4539079654d4a8c838b8bfd105a9db0",
+        "password":"p674d15e7a08e4a8fb8685708ccd87dfc"
+      }
+    }
+  ]
 }
 """
 
